f72acc5912ba41f42b6da0d5649593379d0e046a
hsameer12345 <hsameer@nisum.com>
2022-07-01 19:21:45 +0500

    .compareto method

---
+++ src/main/java/part3/Comparetomethod.java
@@ -0,0 +1,17 @@
+package part3;
+
+public class Comparetomethod {
+    public static void main(String args[]) {
+
+        String str1 = "sameer";
+        String str2 = "sameer";
+        String str3 = "ahmed";
+        String str4 = "mutavalli";
+
+
+
+            System.out.println(str1.compareTo(str2));
+            System.out.println(str3.compareTo(str4)) ;
+
+    }
+}
