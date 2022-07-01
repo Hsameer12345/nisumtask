bf787ca4a7eb3dee1cea791181168d9f6d422801
hsameer12345 <hsameer@nisum.com>
2022-07-01 19:08:08 +0500

    capitalized class

---
+++ src/main/java/part3/Capitalized.java
@@ -0,0 +1,15 @@
+package part3;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Capitalized {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input =  scanner.next();
+
+        System.out.println(input.toUpperCase());
+
+    }
+}
