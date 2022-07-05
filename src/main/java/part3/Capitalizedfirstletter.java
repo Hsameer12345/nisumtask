554d083e7efdf7b2a59660e83b6a1c5af0e89720
hsameer12345 <hsameer@nisum.com>
2022-07-05 19:02:56 +0500

    no message

---
+++ src/main/java/part3/Capitalizedfirstletter.java
@@ -0,0 +1,24 @@
+package part3;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Capitalizedfirstletter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input =  scanner.next();
+        String firstltr = input.substring(0,1);
+        String ltr = input.substring(1);
+
+        firstltr = firstltr.toUpperCase();
+
+        String capitalizeFirstLetter = firstltr + ltr;
+
+
+        System.out.println(capitalizeFirstLetter);
+
+
+
+    }
+}
