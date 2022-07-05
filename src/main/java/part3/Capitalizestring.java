54a54fb05a6429baf1d2d12670655f0899c61882
hsameer12345 <hsameer@nisum.com>
2022-07-05 14:03:01 +0500

    Capitalize string

---
+++ src/main/java/part3/Capitalizestring.java
@@ -0,0 +1,16 @@
+package part3;
+
+import java.util.Scanner;
+
+public  class Capitalizestring {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.next();
+
+        System.out.println(input.toUpperCase());
+
+    }
+}
