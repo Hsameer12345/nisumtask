0415d4bcc054ddb88c59125f283ea3e2bc8e7b8b
hsameer12345 <hsameer@nisum.com>
2022-07-07 14:45:53 +0500

    Exaption handling

---
+++ src/main/java/part3/Exaptionhandling.java
@@ -0,0 +1,22 @@
+package part3;
+
+import java.util.Scanner;
+
+public class Exaptionhandling {
+    public static void main(String[] args) {
+
+      try {
+          Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the first number");
+        int n1 = input.nextInt();
+        System.out.println("Enter the second number");
+        int n2 = input.nextInt();
+
+        int sum = n1%n2;
+        System.out.println(sum);
+      } catch (Exception e) {
+          System.out.println("Invalid value");
+      }
+    }
+}
