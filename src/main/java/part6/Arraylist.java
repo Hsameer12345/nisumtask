c35b166184f2f995270c82188fc0baa0337e8973
hsameer12345 <hsameer@nisum.com>
2022-08-04 17:02:35 +0500

    arraylist sorting by collection method

--- src/main/java/part6/Arraylist.java
+++ src/main/java/part6/Arraylist.java
@@ -1,2 +1,35 @@
-package part6;public class Arraylist {
+package part6;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class Arraylist {
+    public static void main(String[] args) {
+
+        ArrayList <String> arrayList = new ArrayList<>();
+
+        arrayList.add("sameer");
+        arrayList.add("saeed");
+        arrayList.add("ahmed");
+        arrayList.add("mutavalli");
+        Collections.sort(arrayList);
+        System.out.println(arrayList);
+
+        System.out.println(Collections.binarySearch(arrayList,"sameer"));;
+
+
+    
+        String element = "sameer";
+
+        for (int i =0; i<arrayList.size(); i++){
+            if (element == arrayList.get(i)){
+
+                System.out.println("element is found with the name of:" + arrayList.get(i));
+
+                break;
+            }
+        }
+
+    }
 }
