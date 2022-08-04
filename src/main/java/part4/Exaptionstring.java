e4d0acde54b4e68e2fa3a9ce6138b9e35433f14a
hsameer12345 <hsameer@nisum.com>
2022-08-04 17:57:54 +0500

    Exaption handling

---
+++ src/main/java/part4/Exaptionstring.java
@@ -0,0 +1,21 @@
+package part4;
+
+public class Exaptionstring {
+
+
+
+    public static void main(String[] args) {
+
+        String str1=null;
+
+        try {
+            System.out.println(str1.length());
+
+        } catch (Exception e) {
+
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
+
